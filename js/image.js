7bee3a488e0a44cbe72c93f461e423038a3b86e0
Quan Nguyen <nmquanvn@users.noreply.github.com>
2021-03-06 15:53:00 +0700

    'ajax'

--- js/image.js
+++ js/image.js
@@ -3,12 +3,12 @@ function readURL(input) {
     var reader = new FileReader();
 
     reader.onload = function (e) {
-      $('.image-upload-wrap').hide();
+      $(".image-upload-wrap").hide();
 
-      $('.file-upload-image').attr('src', e.target.result);
-      $('.file-upload-content').show();
+      $(".file-upload-image").attr("src", e.target.result);
+      $(".file-upload-content").show();
 
-      $('.image-title').html(input.files[0].name);
+      $(".image-title").html(input.files[0].name);
     };
 
     reader.readAsDataURL(input.files[0]);
@@ -18,13 +18,40 @@ function readURL(input) {
 }
 
 function removeUpload() {
-  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-  $('.file-upload-content').hide();
-  $('.image-upload-wrap').show();
+  $(".file-upload-input").replaceWith($(".file-upload-input").clone());
+  $(".file-upload-content").hide();
+  $(".image-upload-wrap").show();
 }
-$('.image-upload-wrap').bind('dragover', function () {
-  $('.image-upload-wrap').addClass('image-dropping');
+$(".image-upload-wrap").bind("dragover", function () {
+  $(".image-upload-wrap").addClass("image-dropping");
 });
-$('.image-upload-wrap').bind('dragleave', function () {
-  $('.image-upload-wrap').removeClass('image-dropping');
+$(".image-upload-wrap").bind("dragleave", function () {
+  $(".image-upload-wrap").removeClass("image-dropping");
+});
+
+$("#btnUpload").on("click", function () {
+  var formData = new FormData();
+  formData.append("file", $("#img").val());
+  $.ajax({
+    url: "http://localhost:8880",
+    type: "POST",
+    dataType: false,
+    contentType: false,
+    data: formData,
+  })
+    .done(function (res) {
+      alert(res.cancer);
+      // console.log(res);
+      // Swal.fire({
+      //   title: "Error!",
+      //   text: "Do you want to continue. " + JSON.stringify(res),
+      //   icon: "error",
+      //   confirmButtonText: "Cool",
+      // });
+    })
+    .fail(function (jqXHR, textStatus, err) {
+      console.error(jqXHR);
+      console.error(textStatus);
+      console.error(err);
+    });
 });
